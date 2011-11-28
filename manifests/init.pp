e4aa1fb4d39c70155c6991621f8cf9009015fb53
Steven Merrill <steven.merrill@gmail.com>
2011-11-28 11:14:28 -0500

    Add initial commit of Puppet PEAR support.

---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+class pear(
+  $package = $pear::params::package
+) inherits pear::params {
+
+  # Install the PEAR package.
+  package { $package:
+    ensure => installed,
+  }
+
+  # Add a define to allow installing PEAR packages.
+  define pear::package( 
+    $package,
+    $repository = "pear.php.net",
+  ) {
+    include pear
+
+    package { "pear-${repository}-${package}":
+      ensure => installed,
+      provider => "pear",
+      source => "${repository}/${package}",
+    }
+  }
+}
+

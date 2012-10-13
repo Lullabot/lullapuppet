7a57810d90268e02c8928df4bb453994aaad49cb
Andrew Berry <deviantintegral@gmail.com>
2012-10-13 11:31:10 -0400

    Import the MySQL module.

---
+++ mysql/manifests/server.pp
@@ -0,0 +1,46 @@
+class mysql::server (
+        $backup             = true,
+        $backup_retention   = 30,
+        $backup_hour        = 2,
+        $backup_s3_bucket   = false,
+        ) { 
+
+    package { ['mysql-server', 'mysql-client']:
+        ensure  => present,
+    }
+
+    service { 'mysql':
+        ensure  => running,
+        enable  => true,
+        require => Package['mysql-server'],
+    }
+
+    file { '/usr/local/bin/mysqltuner.pl':
+        ensure  => present,
+        source  => 'puppet:///modules/mysql/usr/local/bin/mysqltuner.pl',
+        mode    => '0755',
+    }
+
+    file { '/usr/local/bin/mysqlbackup':
+        ensure  => present,
+        source  => 'puppet:///modules/mysql/usr/local/bin/mysqlbackup',
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0755',
+    }
+
+    cron { 'mysqlbackup':
+        ensure  => $backup ? {
+            true    => 'present',
+            default => 'absent',
+        },
+        user    => 'root',
+        command => $backup_s3_bucket ? {
+            false   => "/usr/local/bin/mysqlbackup -r ${backup_retention}",
+            default => "/usr/local/bin/mysqlbackup -r ${backup_retention} -s ${backup_s3_bucket}",
+        },
+        hour    => $backup_hour,
+        minute  => 0,
+    }
+
+}

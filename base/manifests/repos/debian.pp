06e6066e48306b327083bba2d24275e05b63b5a6
Andrew Berry <deviantintegral@gmail.com>
2013-01-14 15:28:46 -0500

    Remove apt.lullabot.com.

--- base/manifests/repos/debian.pp
+++ base/manifests/repos/debian.pp
@@ -2,15 +2,6 @@ class base::repos::debian {
     
     $dist = downcase($lsbdistid)
 
-    # Apt Sources
-    apt::source { 'lullabot':
-        location    => "http://apt.lullabot.com/${dist}",
-        release     => $lsbdistcodename,
-        repos       => 'main',
-        key         => '62A68384',
-        key_source  => 'http://apt.lullabot.com/lullabot.com.key',
-    }
-
     # Apt Configuration
     file { '/etc/apt/apt.conf.d/10periodic':
         ensure  => present,

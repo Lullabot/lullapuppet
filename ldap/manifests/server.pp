cb8762e4ea8e1deb385b11d07a8fa0a0ad8a10f9
Ben Chavet <ben.chavet@lullabot.com>
2013-03-11 16:10:58 -0500

    new backup strategy, currently being tested internally

--- ldap/manifests/server.pp
+++ ldap/manifests/server.pp
@@ -1,10 +1,5 @@
-class ldap::server (
-        $backup             = true,
-        $backup_retention   = 30,
-        $backup_hour        = 2,
-        $backup_s3_bucket   = false,
-        ) {
-    
+class ldap::server {
+
     package { 'slapd':
         ensure => present,
     }
@@ -13,37 +8,6 @@ class ldap::server (
         ensure  => running,
         enable  => true,
         require => Package['slapd'],
-    }
-
-    file { '/usr/local/bin/ldapbackup':
-        ensure => present,
-        source => 'puppet:///modules/ldap/usr/local/bin/ldapbackup',
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0755',
-    }
-
-    cron { 'ldapbackup':
-        ensure  => $backup ? {
-            true    => 'present',
-            default => 'absent',
-        },
-        user    => 'root',
-        command => "/usr/local/bin/ldapbackup -r ${backup_retention}",
-        hour    => $backup_hour,
-        minute  => 0,
-    }
-
-    cron { 'ldapbackup-s3':
-        ensure => $backup_s3_bucket ? {
-            false   => 'absent',
-            default => 'present',
-        },
-        user    => 'root',
-        command => "/usr/local/bin/ldapbackup -r ${backup_retention} -s ${backup_s3_bucket}",
-        hour    => 4,
-        minute  => 0,
-        weekday => 0,
     }
 
 }

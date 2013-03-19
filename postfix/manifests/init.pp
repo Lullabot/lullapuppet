91adebf8242039d00cfe92f30fe732abc30456e3
Ben Chavet <ben.chavet@lullabot.com>
2013-03-19 12:12:11 -0500

    add some basic postfix configuration to the module

--- postfix/manifests/init.pp
+++ postfix/manifests/init.pp
@@ -1,4 +1,7 @@
-class postfix {
+class postfix (
+        $myhostname = $::fqdn,
+        $aliases    = {},
+    ) {
 
     package { 'postfix':
         ensure  => present,
@@ -8,6 +11,27 @@ class postfix {
         ensure  => running,
         enable  => true,
         require => Package['postfix'],
+    }
+
+    file { '/etc/postfix/main.cf':
+        ensure  => present,
+        content => template('postfix/etc/postfix/main.cf.erb'),
+        notify  => Service['postfix'],
+        owner   => 'root',
+        group   => 'root',
+        mode    => '0644',
+    }
+
+    file { '/etc/aliases':
+        ensure      => present,
+        content     => template('postfix/etc/aliases.erb'),
+        notify      => Exec['newaliases'],
+    }
+
+    exec { 'newaliases':
+		command     => '/usr/bin/newaliases',
+        refreshonly => true,
+        require     => Package['postfix'],
     }
 
 }

278b0267285c5fd162087ff61e16538913462592
Ben Chavet <ben.chavet@lullabot.com>
2013-06-25 11:53:27 -0500

    auto-tune mysql

--- mysql/manifests/server.pp
+++ mysql/manifests/server.pp
@@ -1,4 +1,16 @@
-class mysql::server {
+class mysql::server (
+        $max_allowed_packet             = '64M',
+        $skip_name_resolve              = true,
+        $max_connections                = 100,
+        $table_open_cache               = 400,
+        $default_storage_engine         = 'innodb',
+        $innodb_flush_method            = 'O_DIRECT',
+        $innodb_flush_log_at_trx_commit = 2,
+        $innodb_log_buffer_size         = '8M',
+        $innodb_log_file_size           = '256M',
+        $innodb_file_per_table          = true,
+        $innodb_buffer_pool_size        = undef,
+    ) {
 
     include mysql::client
 
@@ -16,6 +28,18 @@ class mysql::server {
         ensure  => present,
         source  => 'puppet:///modules/mysql/usr/local/bin/mysqltuner.pl',
         mode    => '0755',
+    }
+
+    file { '/etc/mysql/my.cnf':
+        ensure  => present,
+        content => template('mysql/etc/mysql/my.cnf.erb'),
+        require => Package['mysql-server'],
+        notify  => Exec['rm -f /var/lib/mysql/ib_logfile*'],
+    }
+
+    exec { 'rm -f /var/lib/mysql/ib_logfile*':
+        refreshonly => true,
+        notify      => Service['mysql'],
     }
 
 }
